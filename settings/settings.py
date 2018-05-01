f8d483598d0ccf51a4dc4481ff0d41bad1e2eaf7
Gabriel Freites <gabohd@gmail.com>
2018-05-01 18:33:04 +0000

    db sqlite + fixed some bugs

--- settings/settings.py
+++ settings/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '0u@n$)$nxozp2(npz&ew=m6ue!=hex4%3r#i1wrp*75@56+ixu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gabo1208.pythonanywhere.com']
 
 # Application definition
 INSTALLED_APPS = [
@@ -126,17 +126,23 @@ WSGI_APPLICATION = 'settings.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'django_test',
+#        'USER': 'postgres',
+#        'PASSWORD': 'postgres',
+#        'HOST': '127.0.0.1',
+#        'PORT': '5432',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django_test',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_ROOT, 'django_quiniela.db'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
