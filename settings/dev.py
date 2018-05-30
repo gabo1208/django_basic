73083482b4449f4fef44d0eb241fb14b30e9b8b8
Gabriel Freites <gabohd@gmail.com>
2018-05-29 20:14:43 -0400

    changed smtp configuration

--- settings/dev.py
+++ settings/dev.py
@@ -5,5 +5,5 @@ ALLOWED_HOSTS += ['127.0.0.1', 'localhost']
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'memberit'
 EMAIL_HOST_PASSWORD = '..asdf1234'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
